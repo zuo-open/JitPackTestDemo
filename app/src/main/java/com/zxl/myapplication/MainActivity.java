39d78ab11fc285b84f15dbd788177997cebff15f
zuo <1925319474@qq.com>
2022-03-25 19:10:00 +0800

    Jitpack提交

--- app/src/main/java/com/zxl/myapplication/MainActivity.java
+++ app/src/main/java/com/zxl/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.zxl.testmodule.utils.MyUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyUtils.logTest();
     }
 }
